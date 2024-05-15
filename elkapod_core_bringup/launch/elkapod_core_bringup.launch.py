9886314d0209119d58cf6bb11904b65f2d245bf5
Jan Potaszyński <01178244@pw.edu.pl>
2024-05-15 22:55:23 +0200

    Launches simulation and passing argument doesnt work

--- elkapod_core_bringup/launch/elkapod_core_bringup.launch.py
+++ elkapod_core_bringup/launch/elkapod_core_bringup.launch.py
@@ -4,6 +4,7 @@ from launch.substitutions import Command, LaunchConfiguration
 from launch.actions import DeclareLaunchArgument 
 from launch_ros.actions import Node 
 from launch_ros.substitutions import FindPackageShare 
+from launch.substitutions import TextSubstitution
 from launch_ros.parameter_descriptions import ParameterValue 
 from launch.conditions import IfCondition, UnlessCondition, LaunchConfigurationEquals 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -12,6 +13,12 @@ import yaml
 
 
 def generate_launch_description():
+    
+    RorS_launch_arg = DeclareLaunchArgument(
+        "simulation", default_value=TextSubstitution(text="0")
+    )
+    # RorS_launch_arg = str(RorS_launch_arg)
+    
     # Find the packages
     bringup_package = FindPackageShare(package="elkapod_core_bringup").find(
         "elkapod_core_bringup"
@@ -25,7 +32,7 @@ def generate_launch_description():
     comm_package = FindPackageShare(package="elkapod_comm").find(
         "elkapod_comm"
     )
-
+    simulation_package = FindPackageShare(package="elkapod_sim").find("elkapod_sim")
 
 
     # elkapod_kinematics_node = Node(
@@ -44,12 +51,21 @@ def generate_launch_description():
         executable="elkapod_trajectory_generator",
     )
     
-    elkapod_comm_node = Node(
-        package="elkapod_comm",
-        executable="elkapod_comm_server"
+    if (RorS_launch_arg=="0"):
+        elkapod_comm_node = Node(
+            package="elkapod_comm",
+            executable="elkapod_comm_server"
+        )
+    else:
+        elkapod_simulation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(simulation_package, "launch/robot_launch.py")
+        )
     )
 
     # Create the launch description and populate
+    
+    # ld = LaunchDescription(RorS_launch_arg)
     ld = LaunchDescription()
 
     ld.add_action(elkapod_kinematics_launch)
@@ -61,11 +77,14 @@ def generate_launch_description():
         )
     )
     
-    ld.add_action(
-        Node(
-            package="elkapod_comm",
-            executable="elkapod_comm_server"
+    if (RorS_launch_arg=="0"):
+        ld.add_action(
+            Node(
+                package="elkapod_comm",
+                executable="elkapod_comm_server"
+            )
         )
-    )
+    else:
+        ld.add_action(elkapod_simulation_launch)
     
     return ld
