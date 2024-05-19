59c29aa38e6920fb2884a230b0596264f9679f5a
Piotr Patek <piotrpatek17@gmail.com>
2024-05-19 17:33:24 +0200

    Removed elkapod_msgs, probably fixed elkapod_core_bringup

--- elkapod_core_bringup/launch/elkapod_core_bringup.launch.py
+++ elkapod_core_bringup/launch/elkapod_core_bringup.launch.py
@@ -1,90 +1,51 @@
-import os 
-from launch import LaunchDescription 
-from launch.substitutions import Command, LaunchConfiguration 
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument 
-from launch_ros.actions import Node 
-from launch_ros.substitutions import FindPackageShare 
-from launch.substitutions import TextSubstitution
-from launch_ros.parameter_descriptions import ParameterValue 
-from launch.conditions import IfCondition, UnlessCondition, LaunchConfigurationEquals 
+from launch_ros.actions import Node
+from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription
-import yaml
+from ament_index_python import get_package_share_directory
 
 
 def generate_launch_description():
-    
-    RorS_launch_arg = DeclareLaunchArgument(
-        "simulation", default_value=TextSubstitution(text="0")
-    )
-    # RorS_launch_arg = str(RorS_launch_arg)
-    
-    # Find the packages
-    bringup_package = FindPackageShare(package="elkapod_core_bringup").find(
-        "elkapod_core_bringup"
-    )
-    motion_package = FindPackageShare(package="elkapod_motion").find(
-        "elkapod_motion"
-    )
-    trajectory_package = FindPackageShare(package="elkapod_trajectory_generator").find(
-        "elkapod_trajectory_generator"
-    )
-    comm_package = FindPackageShare(package="elkapod_comm").find(
-        "elkapod_comm"
-    )
-    simulation_package = FindPackageShare(package="elkapod_sim").find("elkapod_sim")
 
-
-    # elkapod_kinematics_node = Node(
-    #     package="elkapod_motion",
-    #     executable="elkapod_kinematics",
-    # )
-    
-    elkapod_kinematics_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(motion_package, "launch/elkapod_motion_launch.py")
-        )
-    )
-    
-    elkapod_trajectory_gen_node = Node(
-        package="elkapod_trajectory_generator",
-        executable="elkapod_trajectory_generator",
-    )
-    
-    if (RorS_launch_arg=="0"):
-        elkapod_comm_node = Node(
-            package="elkapod_comm",
-            executable="elkapod_comm_server"
-        )
-    else:
-        elkapod_simulation_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(simulation_package, "launch/robot_launch.py")
-        )
-    )
-
-    # Create the launch description and populate
-    
-    # ld = LaunchDescription(RorS_launch_arg)
     ld = LaunchDescription()
+    ld.add_action(
+        DeclareLaunchArgument(name="sim", default_value="true", choices=["true", "false"])
+    )
 
-    ld.add_action(elkapod_kinematics_launch)
+    elkapod_motion_pkg_prefix = get_package_share_directory('elkapod_motion')
+    elkapod_motion_handler_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [elkapod_motion_pkg_prefix, '/launch/elkapod_motion_launch.py']),
+        launch_arguments={}.items()
+    )
+
+    elkapod_sim_pkg_prefix = get_package_share_directory('elkapod_sim')
+    elkapod_sim_handler_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [elkapod_sim_pkg_prefix, '/launch/robot_launch.py']),
+        launch_arguments={}.items(),
+        condition=IfCondition(LaunchConfiguration("sim"))
+    )
+
+    ld.add_action(elkapod_motion_handler_launch)
+    ld.add_action(elkapod_sim_handler_launch)
+
+    ld.add_action(
+        Node(
+            package="elkapod_comm",
+            executable="elkapod_comm_server",
+            condition=UnlessCondition(LaunchConfiguration("sim"))
+        )
+    )
 
     ld.add_action(
         Node(
             package="elkapod_trajectory_generator",
-            executable="elkapod_trajectory_generator"
+            executable="elkapod_trajectory_generator",
         )
     )
-    
-    if (RorS_launch_arg=="0"):
-        ld.add_action(
-            Node(
-                package="elkapod_comm",
-                executable="elkapod_comm_server"
-            )
-        )
-    else:
-        ld.add_action(elkapod_simulation_launch)
-    
+
     return ld
