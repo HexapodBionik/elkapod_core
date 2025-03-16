02b3e3fae153f24b231070ae7e558ce23e06d3ac
Piotr Patek <piotrpatek17@gmail.com>
2025-03-16 21:42:05 +0100

    Created elkapod_gait_controller ros2 package

---
+++ elkapod_gait_controller/include/elkapod_gait_controller/elkapod_gait_controller.hpp
@@ -0,0 +1,16 @@
+
+//
+// Created by Piotr Patek.
+//
+// Copyright (c) 2025.
+// Elkapod Bionik, Warsaw University of Technology. All rights reserved.
+//
+
+#ifndef ELKAPOD_GAIT_CONTROLLER_HPP
+#define ELKAPOD_GAIT_CONTROLLER_HPP
+
+#include "controller_interface/controller_interface.hpp"
+
+
+
+#endif //ELKAPOD_GAIT_CONTROLLER_HPP
