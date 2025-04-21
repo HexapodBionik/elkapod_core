(#17) Fixed unstable base height during walking by adding smoothstep function for scaling in Z function of swing phase

elkapod_gait_gen_py/elkapod_gait_gen_py/elkapod_gait.py
@@ -47,7 +47,7 @@ class ElkapodGait(Node):
         
 
         self._current_velocity = np.array([0.0, 0.0, 0.0])  # Linear vx, vy and angular wz
-        self._leg_clock_freq = 50                   # in Hz
+        self._leg_clock_freq = 10                   # in Hz
         self._cycle_time = 2.0                      # in seconds
         self._min_cycle_time = 0.5
         self._step_length = 0.1
@@ -80,6 +80,7 @@ class ElkapodGait(Node):
         self._base_traj.init()
         self.changeGait(self._gait_type)
         self._leg_clock_timer.reset()
+        self.get_logger().info("Elkapod gait generator prototype initialized!")
 
     def velocityTopicCallback(self, msg: Twist):
         self._current_velocity[0] = msg.linear.x

elkapod_gait_gen_py/elkapod_gait_gen_py/elkapod_leg_path.py
@@ -24,7 +24,12 @@ class ElkapodLegPathBase:
 
         self._x_func_swing = lambda s: -self._step_length / 2 + self._step_length * s
         self._y_func_swing = lambda s: 0.0
-        self._z_func_swing = lambda s: R * np.sin(np.pi * s) - h
+
+        # Old
+        #self._z_func_swing = lambda s: R * np.sin(np.pi * s) - h
+
+        # New (smootherstep - https://en.wikipedia.org/wiki/Smoothstep)
+        self._z_func_swing = lambda s: R * np.sin(np.pi * (10*pow(s, 3)-15*pow(s, 4)+6*pow(s, 5))) - h
 
 
     def __call__(self, *args, **kwargs):