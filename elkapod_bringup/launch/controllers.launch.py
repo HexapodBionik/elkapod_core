b311271b5f3afeded11d47407ca28dfe417a1f40
Piotr Patek <piotrpatek17@gmail.com>
2025-12-22 22:51:34 +0100

    Update

--- elkapod_bringup/launch/controllers.launch.py
+++ elkapod_bringup/launch/controllers.launch.py
@@ -4,13 +4,15 @@ from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
 from launch.conditions import UnlessCondition
 from launch_ros.actions import Node
+
+
 def generate_launch_description():
     sim_mode = DeclareLaunchArgument(
-        'sim_mode',
-        default_value='false',
-        description='Launch hardware or simulation system'
+        "sim_mode",
+        default_value="false",
+        description="Launch hardware or simulation system",
     )
-        
+
     robot_controllers = PathJoinSubstitution(
         [
             FindPackageShare("elkapod_description"),
@@ -33,52 +35,56 @@ def generate_launch_description():
         parameters=[robot_controllers],
         output="both",
         emulate_tty=True,
-        condition=UnlessCondition(LaunchConfiguration("sim_mode"))
+        condition=UnlessCondition(LaunchConfiguration("sim_mode")),
     )
 
-    controllers = TimerAction(period=5.0, actions=[
-        Node(
-            package="controller_manager",
-            executable="spawner",
-            arguments=["elkapod_ik_controller"],
-            output='screen',
-            emulate_tty=True
-        ),
-        Node(
-            package="controller_manager",
-            executable="spawner",
-            arguments=["imu_broadcaster"],
-            output='screen',
-            emulate_tty=True,
-            condition=UnlessCondition(LaunchConfiguration("sim_mode"))
-        ),
-        Node(
-            package="controller_manager",
-            executable="spawner",
-            arguments=["joint_state_broadcaster"],
-            output='screen',
-            emulate_tty=True
-        )
-    ])
-
-    controllers_second_wave = TimerAction(period=8.0, actions=[
-    Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "elkapod_gait_controller",
-            "--inactive",
-            "--param-file", robot_controllers,
-            "--param-file", motion_manager_config,
+    controllers = TimerAction(
+        period=5.0,
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["elkapod_ik_controller"],
+                output="screen",
+                emulate_tty=True,
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["imu_broadcaster"],
+                output="screen",
+                emulate_tty=True,
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["joint_state_broadcaster"],
+                output="screen",
+                emulate_tty=True,
+            ),
         ],
-        output='screen',
-        emulate_tty=True
     )
-])
 
-    return LaunchDescription([
-        sim_mode,
-        control_node,
-        controllers,
-        controllers_second_wave
-    ])
+    controllers_second_wave = TimerAction(
+        period=8.0,
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[
+                    "elkapod_gait_controller",
+                    "--inactive",
+                    "--param-file",
+                    robot_controllers,
+                    "--param-file",
+                    motion_manager_config,
+                ],
+                output="screen",
+                emulate_tty=True,
+            )
+        ],
+    )
+
+    return LaunchDescription(
+        [sim_mode, control_node, controllers, controllers_second_wave]
+    )
