c4c769cfe8f5938aa0ac3864b1575ca2c7a3286b
Damian <baraniak.damian@gmail.com>
2025-12-30 23:54:17 +0100

    added some xacros adjusting lidar work in simulation

--- elkapod_bringup/launch/rsp.launch.py
+++ elkapod_bringup/launch/rsp.launch.py
@@ -8,11 +8,15 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     sim_mode = LaunchConfiguration('sim_mode')
-
+    lidar_mode = LaunchConfiguration('lidar_mode')
+    lidar_rate = LaunchConfiguration('lidar_rate')
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('elkapod_description'))
     xacro_file = os.path.join(pkg_path, 'urdf','elkapod.urdf.xacro')
-    robot_description_config = Command(['xacro ', xacro_file, ' sim_mode:=', sim_mode])
+    robot_description_config = Command(['xacro ', xacro_file,
+                                        ' sim_mode:=', sim_mode,
+                                        ' lidar_mode:=', lidar_mode,
+                                        ' lidar_rate:=', lidar_rate])
 
     params = {'robot_description': robot_description_config, 'sim_mode': sim_mode}
     node_robot_state_publisher = Node(
@@ -22,10 +26,19 @@ def generate_launch_description():
         parameters=[params]
     )
 
+
     return LaunchDescription([
         DeclareLaunchArgument(
             'sim_mode',
             default_value='false',
             description='Use sim time if true'),
+        DeclareLaunchArgument(
+        'lidar_mode', default_value='minimal',
+        description='LiDAR config: off, minimal, full'
+        ),
+        DeclareLaunchArgument(
+            'lidar_rate', default_value='3',
+            description='Update rate for LiDAR'
+        ),
         node_robot_state_publisher
     ])
